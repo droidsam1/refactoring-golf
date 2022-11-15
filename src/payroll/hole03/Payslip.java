b6f2bd51f27f67311a0b2c6fffc4dbdca8028a9f
droidsam1 <droidsam@gmail.com>
2022-11-15 21:17:59 +0100

    refactor: automated refactor

--- src/payroll/hole03/Payslip.java
+++ src/payroll/hole03/Payslip.java
@@ -8,10 +8,15 @@ public class Payslip {
     }
 
     public double getNet() {
+        double taxDeductions = getTaxDeductions();
+        return grossSalary - taxDeductions;
+    }
+
+    private double getTaxDeductions() {
         final double grossSalarySubjectToLowerTaxBracket = Math.max(Math.min(grossSalary, 20000.0) - 5000, 0.0);
         final double grossSalarySubjectToMediumTaxBracket = Math.max(Math.min(grossSalary, 40000) - 20000, 0.0);
         final double grossSalarySubjectToUpperTaxBracket = Math.max(grossSalary - 40000, 0.0);
         double taxDeductions = grossSalarySubjectToLowerTaxBracket * 0.1 + grossSalarySubjectToMediumTaxBracket * 0.2 + grossSalarySubjectToUpperTaxBracket * 0.4;
-        return grossSalary - taxDeductions;
+        return taxDeductions;
     }
 }
