2d4430c06f78bd6334396949a5bd71358838a003
droidsam1 <droidsam@gmail.com>
2022-11-15 22:40:59 +0100

    reset hole 4 to start from sractch

--- src/payroll/hole04/Payslip.java
+++ src/payroll/hole04/Payslip.java
@@ -2,14 +2,20 @@ package payroll.hole04;
 
 public class Payslip {
     private final double grossSalary;
-    private final TaxCalculator taxCalculator;
 
-    public Payslip(final double grossSalary, final TaxCalculator taxCalculator) {
+    public Payslip(final double grossSalary) {
         this.grossSalary = grossSalary;
-        this.taxCalculator = taxCalculator;
     }
 
     public double getNet() {
-        return grossSalary - taxCalculator.taxFor(grossSalary);
+        double taxDeductions = getTaxDeductions();
+        return grossSalary - taxDeductions;
+    }
+
+    private double getTaxDeductions() {
+        final double grossSalarySubjectToLowerTaxBracket = Math.max(Math.min(grossSalary, 20000.0) - 5000, 0.0);
+        final double grossSalarySubjectToMediumTaxBracket = Math.max(Math.min(grossSalary, 40000) - 20000, 0.0);
+        final double grossSalarySubjectToUpperTaxBracket = Math.max(grossSalary - 40000, 0.0);
+        return grossSalarySubjectToLowerTaxBracket * 0.1 + grossSalarySubjectToMediumTaxBracket * 0.2 + grossSalarySubjectToUpperTaxBracket * 0.4;
     }
 }
