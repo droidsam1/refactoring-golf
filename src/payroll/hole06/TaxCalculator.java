08d6ed8b216bcf17e5ceb5348b74b87fc1ac9c6f
droidsam1 <droidsam@gmail.com>
2022-11-16 20:57:39 +0100

    refactor: automated refactor

--- src/payroll/hole06/TaxCalculator.java
+++ src/payroll/hole06/TaxCalculator.java
@@ -2,10 +2,14 @@ package payroll.hole06;
 
 public class TaxCalculator {
 	public double taxFor(final double grossSalary) {
-		final double lowerTaxBracketGross = Math.max(grossToTaxInBandsBelowCurrent(grossSalary, 20000.0) - 5000, 0.0);
-		final double middleTaxBracketGross = Math.max(grossToTaxInBandsBelowCurrent(grossSalary, 40000) - 20000, 0.0);
-		final double upperTaxBracketGross = Math.max(grossSalary - 40000, 0.0);
+		final double lowerTaxBracketGross = grossToTaxInBand(grossToTaxInBandsBelowCurrent(grossSalary, 20000.0), 5000);
+		final double middleTaxBracketGross = grossToTaxInBand(grossToTaxInBandsBelowCurrent(grossSalary, 40000), 20000);
+		final double upperTaxBracketGross = grossToTaxInBand(grossSalary, 40000);
 		return lowerTaxBracketGross * 0.1 + middleTaxBracketGross * 0.2 + upperTaxBracketGross * 0.4;
+	}
+
+	private static double grossToTaxInBand(double grossSalary, int x) {
+		return Math.max(grossSalary - x, 0.0);
 	}
 
 	private static double grossToTaxInBandsBelowCurrent(double grossSalary, double b) {
