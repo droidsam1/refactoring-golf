5c6ae7234e702c9970e41c2959f1e22c5b6a6e25
droidsam1 <droidsam@gmail.com>
2022-11-16 20:09:51 +0100

    refactor: automated refactor

--- src/payroll/hole05/PayslipTest.java
+++ src/payroll/hole05/PayslipTest.java
@@ -5,36 +5,39 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class PayslipTest {
+
+    public static final double DELTA = 1e-6;
+
     @Test
     public void taxIsZeroIfGrossIsBelowTaxFreeLimit() {
         final Payslip payslip = new Payslip(5000, new TaxCalculator());
-        assertEquals(5000, payslip.getNet(), 1e-6);
+        assertEquals(5000, payslip.getNet(), DELTA);
     }
 
     @Test
     public void taxOnAmountInLowerTaxBracketExcludesTaxFreeLimit() {
         final Payslip payslip = new Payslip(10000, new TaxCalculator());
-        assertEquals(9500, payslip.getNet(), 1e-6);
+        assertEquals(9500, payslip.getNet(), DELTA);
 
         final Payslip payslip2 = new Payslip(20000, new TaxCalculator());
-        assertEquals(18500, payslip2.getNet(), 1e-6);
+        assertEquals(18500, payslip2.getNet(), DELTA);
     }
 
     @Test
     public void taxOnAmountInMiddleTaxBracketIsSumOfLowerTaxBracketAmountAndAdditionalMiddleTaxBracketAmount() {
         final Payslip payslip = new Payslip(25000, new TaxCalculator());
-        assertEquals(22500, payslip.getNet(), 1e-6);
+        assertEquals(22500, payslip.getNet(), DELTA);
 
         final Payslip payslip2 = new Payslip(40000, new TaxCalculator());
-        assertEquals(34500, payslip2.getNet(), 1e-6);
+        assertEquals(34500, payslip2.getNet(), DELTA);
     }
 
     @Test
     public void taxOnAmountInUpperTaxBracketIsSumOfLowerTaxBracketAmountAndMiddleTaxBracketAmountAndAdditionalUpperTaxBracketAmount() {
         final Payslip payslip = new Payslip(50000, new TaxCalculator());
-        assertEquals(40500, payslip.getNet(), 1e-6);
+        assertEquals(40500, payslip.getNet(), DELTA);
 
         final Payslip payslip2 = new Payslip(60000, new TaxCalculator());
-        assertEquals(46500, payslip2.getNet(), 1e-6);
+        assertEquals(46500, payslip2.getNet(), DELTA);
     }
 }
