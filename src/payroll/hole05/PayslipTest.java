85a6c7d809cdfb437b0907e2eea7770b17bcf444
droidsam1 <droidsam@gmail.com>
2022-11-16 20:16:48 +0100

    refactor: automated refactor

--- src/payroll/hole05/PayslipTest.java
+++ src/payroll/hole05/PayslipTest.java
@@ -8,38 +8,36 @@ public class PayslipTest {
 
     public static final double DELTA = 1e-6;
 
-    @Test
-    public void taxIsZeroIfGrossIsBelowTaxFreeLimit() {
-        int grossSalary = 5000;
-        int expectedNet = 5000;
+    private static void assertNetGivenGross(int grossSalary, int expectedNet) {
         final Payslip payslip = new Payslip(grossSalary, new TaxCalculator());
         assertEquals(expectedNet, payslip.getNet(), DELTA);
     }
 
     @Test
-    public void taxOnAmountInLowerTaxBracketExcludesTaxFreeLimit() {
-        final Payslip payslip = new Payslip(10000, new TaxCalculator());
-        assertEquals(9500, payslip.getNet(), DELTA);
+    public void taxIsZeroIfGrossIsBelowTaxFreeLimit() {
+        int grossSalary = 5000;
+        int expectedNet = 5000;
+        assertNetGivenGross(grossSalary, expectedNet);
+    }
 
-        final Payslip payslip2 = new Payslip(20000, new TaxCalculator());
-        assertEquals(18500, payslip2.getNet(), DELTA);
+    @Test
+    public void taxOnAmountInLowerTaxBracketExcludesTaxFreeLimit() {
+        assertNetGivenGross(10000, 9500);
+
+        assertNetGivenGross(20000, 18500);
     }
 
     @Test
     public void taxOnAmountInMiddleTaxBracketIsSumOfLowerTaxBracketAmountAndAdditionalMiddleTaxBracketAmount() {
-        final Payslip payslip = new Payslip(25000, new TaxCalculator());
-        assertEquals(22500, payslip.getNet(), DELTA);
+        assertNetGivenGross(25000, 22500);
 
-        final Payslip payslip2 = new Payslip(40000, new TaxCalculator());
-        assertEquals(34500, payslip2.getNet(), DELTA);
+        assertNetGivenGross(40000, 34500);
     }
 
     @Test
     public void taxOnAmountInUpperTaxBracketIsSumOfLowerTaxBracketAmountAndMiddleTaxBracketAmountAndAdditionalUpperTaxBracketAmount() {
-        final Payslip payslip = new Payslip(50000, new TaxCalculator());
-        assertEquals(40500, payslip.getNet(), DELTA);
+        assertNetGivenGross(50000, 40500);
 
-        final Payslip payslip2 = new Payslip(60000, new TaxCalculator());
-        assertEquals(46500, payslip2.getNet(), DELTA);
+        assertNetGivenGross(60000, 46500);
     }
 }
