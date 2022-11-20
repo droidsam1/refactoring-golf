6cd792ac80b4ba600cac5cecacb9ca5aef96d319
droidsam1 <droidsam@gmail.com>
2022-11-20 11:04:15 +0100

    refactor: automated refactor

--- src/payroll/hole07/TaxCalculator.java
+++ src/payroll/hole07/TaxCalculator.java
@@ -6,15 +6,15 @@ public class TaxCalculator {
         int upperTaxBracketStart = 40000;
         double upperTaxBracketRate = 0.4;
         TaxBand upperTaxBand = new TaxBand(upperTaxBracketStart, upperTaxBracketRate);
-        final double upperTaxBracketGross = upperTaxBand.grossToTaxInBand(grossSalary, upperTaxBracketStart);
+        final double upperTaxBracketGross = upperTaxBand.grossToTaxInBand(grossSalary);
         int middleTaxBracketStart = 20000;
         double middleTaxBracketRate = 0.2;
         TaxBand middleTaxBand = new TaxBand(middleTaxBracketStart, middleTaxBracketRate);
-        final double middleTaxBracketGross = middleTaxBand.grossToTaxInBand(upperTaxBand.grossToTaxInBandsBelowCurrent(grossSalary), middleTaxBracketStart);
+        final double middleTaxBracketGross = middleTaxBand.grossToTaxInBand(upperTaxBand.grossToTaxInBandsBelowCurrent(grossSalary));
         int lowerTaxBracketStart = 5000;
         double lowerTaxBracketRate = 0.1;
         TaxBand lowerTaxBand = new TaxBand(lowerTaxBracketStart, lowerTaxBracketRate);
-        final double lowerTaxBracketGross = lowerTaxBand.grossToTaxInBand(middleTaxBand.grossToTaxInBandsBelowCurrent(grossSalary), lowerTaxBracketStart);
+        final double lowerTaxBracketGross = lowerTaxBand.grossToTaxInBand(middleTaxBand.grossToTaxInBandsBelowCurrent(grossSalary));
         return lowerTaxBand.taxInBand(lowerTaxBracketGross) + middleTaxBand.taxInBand(middleTaxBracketGross) + upperTaxBand.taxInBand(upperTaxBracketGross);
     }
 
@@ -32,7 +32,7 @@ public class TaxCalculator {
             return lowerTaxBracketGross * taxBracketRate;
         }
 
-        private double grossToTaxInBand(double grossSalary, double taxBracketStart) {
+        private double grossToTaxInBand(double grossSalary) {
             return Math.max(grossSalary - taxBracketStart, 0.0);
         }
 
